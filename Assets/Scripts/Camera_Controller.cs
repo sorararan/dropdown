6bc4f3f62d2966375a0c88908f99d09a435e7ecb
sorararan <masolainota@gmail.com>
2018-05-20 11:44:20 +0900

    create_foundation

---
+++ Assets/Scripts/Camera_Controller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour {
+	GameObject player;
+	Vector3 position;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		position = player.transform.localPosition;
+		position.z -= 10;
+		this.transform.localPosition = position;
+	}
+}
---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+	Rigidbody rb;
+	float speed = 10f;
+	float stopspeed = -20f;
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(rb.velocity.y < stopspeed){
+			rb.velocity = new Vector3(rb.velocity.x, stopspeed, rb.velocity.z);
+		}
+		Debug.Log ("速度ベクトル: " + rb.velocity);
+		if(Input.GetKey(KeyCode.RightArrow)){
+			rb.AddForce(transform.right * speed);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			rb.AddForce(-transform.right * speed);
+		}
+	}
+}
