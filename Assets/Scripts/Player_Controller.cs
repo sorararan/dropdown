832027eacc38f761f48cd18ce07ad899a5b4a30f
sorararan <masolainota@gmail.com>
2018-05-20 14:05:24 +0900

    fix

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -3,24 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Controller : MonoBehaviour {
-	Rigidbody rb;
-	float speed = 10f;
-	float stopspeed = -20f;
+	private Rigidbody rb;
+	private float speed = 10f;
+	private float stopspeed = -20f;
+	private bool stop;
 	// Use this for initialization
 	void Start () {
-		rb = this.GetComponent<Rigidbody>();
+		rb = this.GetComponent<Rigidbody> ();
+		stop = false;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if(rb.velocity.y < stopspeed){
-			rb.velocity = new Vector3(rb.velocity.x, stopspeed, rb.velocity.z);
+		if (rb.velocity.y < stopspeed) {
+			rb.velocity = new Vector3 (rb.velocity.x, stopspeed, rb.velocity.z);
 		}
-		if(Input.GetKey(KeyCode.RightArrow)){
-			rb.AddForce(transform.right * speed);
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rb.AddForce (transform.right * speed);
 		}
-		if(Input.GetKey(KeyCode.LeftArrow)){
-			rb.AddForce(-transform.right * speed);
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			rb.AddForce (-transform.right * speed);
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			if (stop) {
+				rb.constraints = RigidbodyConstraints.None;
+				stop = !stop;
+			} else {
+				rb.constraints = RigidbodyConstraints.FreezeAll;
+				stop = !stop;
+			}
 		}
 	}
 }
